bdc1a038c2c00c3e29bf216ff0900491096fb73e
Sellec <echelonww@gmail.com>
2019-03-30 07:09:31 +0300

    Initial

---
+++ Projects/Application/Modules/Constants.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnUtils.Application.Modules
+{
+    /// <summary>
+    /// Некоторые константы, связанные с модулями.
+    /// </summary>
+    public static class Constants
+    {
+        /// <summary>
+        /// Обозначает разрешение на управление модулем.
+        /// </summary>
+        public const string PermissionManageString = "accessadmin";
+
+        /// <summary>
+        /// Обозначает разрешение для доступа авторизованного пользователя к модулю.
+        /// </summary>
+        public const string PermissionAccessUserString = "accessuser";
+
+        /// <summary>
+        /// Обозначает разрешение на управление модулем.
+        /// </summary>
+        public static readonly Guid PermissionManage = PermissionManageString.GenerateGuid();
+
+        /// <summary>
+        /// Обозначает разрешение для доступа авторизованного пользователя к модулю.
+        /// </summary>
+        public static readonly Guid PermissionAccessUser = PermissionAccessUserString.GenerateGuid();
+    }
+}
