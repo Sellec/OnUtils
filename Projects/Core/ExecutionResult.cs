448dceb5ea5df7c1e821e171a42f054bdf3d7e0e
Sellec <echelonww@gmail.com>
2020-12-29 16:19:08 +0300

    ExecutionResult конвертирован в struct.

--- Projects/Core/ExecutionResult.cs
+++ Projects/Core/ExecutionResult.cs
@@ -1,21 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace OnUtils
+﻿namespace OnUtils
 {
     /// <summary>
     /// Представляет результат выполнения какого-либо метода или операции.
     /// </summary>
-    public class ExecutionResult
+    public struct ExecutionResult
     {
         /// <summary>
         /// </summary>
         public ExecutionResult(bool isSuccess, string message = null)
         {
-            this.IsSuccess = isSuccess;
-            this.Message = message;
+            IsSuccess = isSuccess;
+            Message = message;
         }
 
         /// <summary>
@@ -33,19 +28,30 @@ namespace OnUtils
     /// Представляет результат выполнения какого-либо метода или операции с данными, полученными в процессе выполнения.
     /// </summary>
     /// <typeparam name="TResult">Тип данных, полученных в процессе выполнения.</typeparam>
-    public class ExecutionResult<TResult> : ExecutionResult
+    public struct ExecutionResult<TResult>
     {
         /// <summary>
         /// </summary>
-        public ExecutionResult(bool isSuccess, string message = null, TResult result = default(TResult)) : base(isSuccess, message)
+        public ExecutionResult(bool isSuccess, string message = null, TResult result = default(TResult))
         {
+            IsSuccess = isSuccess;
+            Message = message;
             Result = result;
         }
+
+        /// <summary>
+        /// Признак успешности выполнения.
+        /// </summary>
+        public bool IsSuccess { get; }
+
+        /// <summary>
+        /// Текстовое сообщение об ошибке или об успехе. Может быть пустым.
+        /// </summary>
+        public string Message { get; }
 
         /// <summary>
         /// Данные, полученные в процессе выполнения.
         /// </summary>
         public TResult Result { get; }
     }
-
 }
